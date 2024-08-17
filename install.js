a27bc72a029a6e18d6509a16876ba534dde48508
cocktailpeanut <cocktailpeanuts@proton.me>
2024-08-17 09:11:05 -0400

    update

--- install.js
+++ install.js
@@ -129,6 +129,7 @@ module.exports = async (kernel, info) => {
           "photomaker": "app/models/photomaker"
         },
         "peers": [
+          "https://github.com/pinokiofactory/comfy.git",
           "https://github.com/cocktailpeanutlabs/automatic1111.git",
           "https://github.com/cocktailpeanutlabs/fooocus.git",
           "https://github.com/cocktailpeanutlabs/comfyui.git",
