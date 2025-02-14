f3235f7b11f71bcfc219ffe775fc326b1dd62f12
6Morpheus6 <wanton-decay@gmx.net>
2025-02-14 21:01:56 +0100

    confrim reset

--- pinokio.js
+++ pinokio.js
@@ -175,6 +175,7 @@ module.exports = {
           icon: "fa-regular fa-circle-xmark",
           text: "Reset",
           href: "reset.js",
+          confirm: "Are you sure you want to delete the app?"
         }]
       }
     } else {
