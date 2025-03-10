88bbf0b0cc4e7ac7ecd1c2b284f0a19fda75e9e9
cocktailpeanut <cocktailpeanuts@proton.me>
2025-03-10 16:40:24 -0700

    rtx 50 series handling

--- torch.js
+++ torch.js
@@ -1,17 +1,18 @@
 module.exports = {
   run: [
-//    // windows/linux nvidia (5090)
-//    {
-//      "when": "{{gpu === 'nvidia' && gpu_model && / 50[0-9]+/.test(gpu_model) }}",
-//      "method": "shell.run",
-//      "params": {
-//        "venv": "{{args && args.venv ? args.venv : null}}",
-//        "path": "{{args && args.path ? args.path : '.'}}",
-//        "message": "uv pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128"
-//      },
-//      "next": null
-//    },
-    // windows/linux nvidia (normal)
+    // nvidia 50 series
+    {
+      "when": "{{gpu === 'nvidia' && kernel.gpu_model && / 50.+/.test(kernel.gpu_model) }}",
+      "method": "shell.run",
+      "params": {
+        "venv": "{{args && args.venv ? args.venv : null}}",
+        "path": "{{args && args.path ? args.path : '.'}}",
+        "message": [
+          "uv pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128"
+        ]
+      },
+      "next": null
+    },
     {
       "when": "{{gpu === 'nvidia'}}",
       "method": "shell.run",
